9be0dd34478f5f214d644ca2a30fe4656ddf1d4b
RobertAlblas <robertalblas1993@gmail.com>
2014-09-04 17:08:03 +0200

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit b3902aaa412239fc8402962819e117d470d1f3b2, reversing
    changes made to 133e050651aae977ddd58da2e90b61d4675aa0ce.

--- KB-06/KB-06/KB-06.cpp
+++ KB-06/KB-06/KB-06.cpp
@@ -3,16 +3,11 @@
 
 #include "stdafx.h"
 #include <cstdlib>
-#include "logger.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Logger* logger = Logger::GetLogger("main");
-	logger->SetLogLevel(Logger::DEBUG);
-	logger->Log(Logger::ERR, "Iets");
-	logger->Log(Logger::WARNING, "Iets");
-	logger->Log(Logger::DEBUG, "Iets");
-	logger->Log(Logger::INFO, "Iets");
+	printf("Hello World\n");
+	system("pause"); //Niet cross-platform & algemeen lelijk
 	return 0;
 }
 
