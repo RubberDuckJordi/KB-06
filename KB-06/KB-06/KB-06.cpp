49f282422c656b2e210e49bb3700721de0c61738
ManIkWeet <spamme2010@live.nl>
2014-09-04 17:18:34 +0200

    Window Manager en Window toegevoegd

--- KB-06/KB-06/KB-06.cpp
+++ KB-06/KB-06/KB-06.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include <cstdlib>
 #include "logger.h"
+#include "WindowManager.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -13,6 +14,19 @@ int _tmain(int argc, _TCHAR* argv[])
 	logger->Log(Logger::WARNING, "Iets");
 	logger->Log(Logger::DEBUG, "Iets");
 	logger->Log(Logger::INFO, "Iets");
+
+	WindowManager* wManager = new WindowManager(NULL);
+	wManager->newWindow(NULL, 10, 10, 500, 500);
+	wManager->getLastWindow()->SetTitle("Waarom lees jij deze titel? Het kost je meer tijd dan het waard was!");
+	wManager->newWindow(NULL, 100, 100, 500, 500);
+	wManager->getLastWindow()->SetTitle("Waarom lees jij deze titel? Het kost je meer tijd dan het waard was!");
+
+	while (wManager->hasActiveWindow())
+	{
+		wManager->updateWindows();
+		//Do things like input, game logic, ...
+	}
+
 	return 0;
 }
 
