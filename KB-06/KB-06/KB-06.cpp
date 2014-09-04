79c7713797253b728c74a62c3ad271d19282398b
ManIkWeet <spamme2010@live.nl>
2014-09-04 13:14:08 +0200

    Een stukje hello world, dan hebben we tenminste iets

--- KB-06/KB-06/KB-06.cpp
+++ KB-06/KB-06/KB-06.cpp
@@ -2,10 +2,12 @@
 //
 
 #include "stdafx.h"
-
+#include <cstdlib>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	printf("Hello World\n");
+	system("pause"); //Niet cross-platform & algemeen lelijk
 	return 0;
 }
 
