ad01b998e0bf26f7c7cf21f9c6f7caadfb2d3d08
hammersyko <alinor.hammersyko@gmail.com>
2014-09-05 15:25:24 +0200

    (so i can sync...) start of methods in DirectXRenderer

---
+++ KB-06/KB-06/DirectXRenderer.cpp
@@ -0,0 +1,19 @@
+#include "DirectXRenderer.h"
+
+DirectXRenderer::DirectXRenderer()
+{
+	g_pD3D = NULL;
+	g_pd3dDevice = NULL;
+};
+DirectXRenderer::~DirectXRenderer()
+{
+	if (g_pd3dDevice != NULL)
+	{
+		g_pd3dDevice->Release();
+	}
+
+	if (g_pD3D != NULL)
+	{
+		g_pD3D->Release();
+	}
+};
---
+++ KB-06/KB-06/DirectXRenderer.h
@@ -0,0 +1,35 @@
+#ifndef _DIRECTXRENDERER_H_
+#define _DIRECTXRENDERER_H_
+
+#include <d3dx9.h>
+
+class DirectXRenderer
+{
+public:
+	DirectXRenderer();
+	~DirectXRenderer();
+
+	void InitD3D();
+	void SetRenderState();
+
+	void SetWorldMatrix();
+	void SetViewMatrix();
+	void SetProjectionMatrix();
+
+	void BeginScene();
+	void ClearScene();
+	void PresentScene();
+	void StopScene();
+
+	void CreateVertexBuffer();
+	void CreateIndexBuffer();
+
+	void SetMaterial();
+	void SetTexture();
+
+private:
+	LPDIRECT3D9 g_pD3D;
+	LPDIRECT3DDEVICE9 g_pd3dDevice;
+};
+
+#endif
