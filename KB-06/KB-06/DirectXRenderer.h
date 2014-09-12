02425a08b909c6bd50459235c5e1521b1bc0c75f
Rimmert Koldenhof <groudon1111@gmail.com>
2014-09-12 14:51:35 +0200

    Renderer should be done now. i don't trust him though.

--- KB-06/KB-06/DirectXRenderer.h
+++ KB-06/KB-06/DirectXRenderer.h
@@ -6,6 +6,7 @@
 #include "Renderer.h"
 #include "LoggerPool.h"
 
+
 #pragma comment(lib, "d3dx9.lib")
 #pragma comment(lib, "d3d9.lib")
 
@@ -25,16 +26,16 @@ namespace Renderer
 		void SetProjectionMatrix(MatrixWrapper* ProjectionMatrix);
 
 		void BeginScene();
-		void ClearScene(DWORDWrapper* count, DWORDWrapper* flags, ColorWrapper* color, float z, DWORDWrapper* stencil);
+		void ClearScene(PENGINEDWORD count, PENGINEDWORD flags, PENGINECOLOR color, float z, PENGINEDWORD stencil);
 		void PresentScene(HWND hWnd);
 		void StopScene();
 
-		void CreateVertexBuffer(int heightmapvertex, DWORDWrapper* usage, DWORDWrapper* fvf, PoolWrapper* pool, VertexBufferWrapper* vertexbuffer, HANDLE handle);
-		void CreateIndexBuffer(int length, DWORDWrapper* usage, FormatWrapper* format, PoolWrapper* pool, IndexBufferWrapper* Indexbuffer, HANDLE* handle);
+		void CreateVertexBuffer(int heightmapvertex, PENGINEDWORD usage, PENGINEDWORD fvf, PENGINEPOOL pool, VertexBufferWrapper* vertexbuffer, HANDLE handle);
+		void CreateIndexBuffer(int length, PENGINEDWORD usage, PENGINEFORMAT format, PENGINEPOOL pool, IndexBufferWrapper* Indexbuffer, HANDLE* handle);
 
 		void SetMaterial(MaterialWrapper* wrapper);
 		void SetTexture(TextureWrapper* wrapper);
-		void SetFvF(DWORDWrapper* fvf);
+		void SetFvF(PENGINEDWORD fvf);
 		void SetTransform(int type, MatrixWrapper* wrapper);
 
 		void DrawPrimitive(Resource::Mesh mesh);
