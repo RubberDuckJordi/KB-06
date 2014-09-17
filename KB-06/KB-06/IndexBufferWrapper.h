325234b489089bb8453f96883fd606c363aa676e
Rimmert Koldenhof <groudon1111@gmail.com>
2014-09-17 13:55:23 +0200

    indexbufferwrapper erbij

---
+++ KB-06/KB-06/IndexBufferWrapper.h
@@ -0,0 +1,22 @@
+#ifndef _INDEXBUFFERWRAPPER_H_
+#define _INDEXBUFFERWRAPPER_H_
+
+#include <d3dx9.h>
+
+namespace Renderer
+{
+	class IndexBufferWrapper
+	{
+	public:
+		IndexBufferWrapper(IDirect3DIndexBuffer9** _IndexBuffer);
+		~IndexBufferWrapper();
+		void SetIndexBuffer(IDirect3DIndexBuffer9** _IndexBuffer);
+		IDirect3DIndexBuffer9** GetIndexBuffer();
+
+
+	private:
+		IDirect3DIndexBuffer9** IndexBuffer;
+	};
+}
+
+#endif
