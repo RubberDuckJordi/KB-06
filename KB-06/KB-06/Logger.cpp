692d765951f102e83413f130355d0ec7209826f1
ManIkWeet <spamme2010@live.nl>
2014-09-25 12:18:54 +0200

    Logger is now more efficient and supports infinite messages
    No longer is there a buffer of 1024 characters total, this has been
    replaced by a string stream.
    There is now only a single switch statement per log entry.
    Code is more easily readable.

--- KB-06/KB-06/Logger.cpp
+++ KB-06/KB-06/Logger.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <windows.h>
 #include <ctime>
+#include <sstream>
 /*
 Do NOT directly instaniate this class, use the loggerpool instead!
 The logger defaults to the highest loglevel
@@ -18,67 +19,67 @@ void Logger::Logger::Reset(){
 	logLevel = INFO;
 }
 
-void Logger
-::Logger::NewFile(){
+void Logger::Logger::NewFile(){
 	remove(previousLogFile);
 	rename(logFile, previousLogFile);
 }
+
 void Logger::Logger::Log(int logType, std::string messageString){
-	char* message = new char[messageString.length()+1];
-	strcpy_s(message, messageString.length()+1, messageString.c_str());
+	char* message = new char[messageString.length() + 1];
+	strcpy_s(message, messageString.length() + 1, messageString.c_str());
 	Log(logType, message);
 }
 
 void Logger::Logger::Log(int logType, char* message){
 
-	if (logLevel >= logType && logType > 0){
-		char* entry = BuildLogEntry(logType, message);
-		PrintConsole(logType, entry);
+	if (logLevel >= logType){
+		std::ostringstream oss;
+
+		SYSTEMTIME time;
+		GetLocalTime(&time);
+
+		char* logEntry = new char[16];
+		sprintf_s(logEntry, 16, "[%02d:%02d:%02d.%03d] ", time.wHour, time.wMinute, time.wSecond, time.wMilliseconds);
+		oss << logEntry;
+
+		int color;
+		switch (logType)
+		{
+		case INFO:
+			oss << "<INFO   >: ";
+			color = consoleColorCodeInfo;
+			break;
+		case DEBUG:
+			oss << "<DEBUG  >: ";
+			color = consoleColorCodeDebug;
+			break;
+		case WARNING:
+			oss << "<WARNING>: ";
+			color = consoleColorCodeWarning;
+			break;
+		case ERR:
+			oss << "<ERROR  >: ";
+			color = consoleColorCodeError;
+			break;
+		default:
+			oss << "<NONE   >: ";
+			color = consoleColorCodeNone;
+			break;
+		}
+
+		oss << message << std::endl;
+
 		std::ofstream outfile;
 		outfile.open("log.txt", std::ios_base::app);
-		outfile << entry << "\n";
-	}
-}
+		outfile << oss.str() << std::endl;
 
-void Logger::Logger::PrintConsole(int logType, char* message)
-{
-	int color;
-	switch (logType) {
-	case INFO:
-		color = consoleColorCodeInfo;
-		break;
-	case DEBUG:
-		color = consoleColorCodeDebug;
-		break;
-	case WARNING:
-		color = consoleColorCodeWarning;
-		break;
-	case ERR:
-		color = consoleColorCodeError;
-		break;
+		SetConsoleTextAttribute(consoleHandle, color);
+		std::cout << oss.str();
 	}
-	SetConsoleTextAttribute(consoleHandle, color);
-
-	std::cout << message << std::endl;
 }
 
 void Logger::Logger::SetLogLevel(int newLogLevel){
 	logLevel = newLogLevel;
-}
-
-char* Logger::Logger::BuildLogEntry(int logType, char* message){
-	SYSTEMTIME systemTime;
-	GetLocalTime(&systemTime);
-	char* logTypeString = new char[7];
-	switch (logType) {
-		case INFO: logTypeString = "INFO   ";	break;
-		case DEBUG:logTypeString = "DEBUG  "; break;
-		case WARNING:logTypeString = "WARNING"; break;
-		case ERR: logTypeString = "ERROR  "; break;
-	}
-	char* logEntry = new char[1024 + sizeof(message) + sizeof(logTypeString)];
-	sprintf_s(logEntry, 1024, "[%02d:%02d:%02d.%03d] %s %s", systemTime.wHour, systemTime.wMinute, systemTime.wSecond, systemTime.wMilliseconds, logTypeString, message);
-	return logEntry;
 }
 
 void Logger::Logger::LogMemoryDump(int logType, void* const p_address, const int p_size, char* const p_name)
@@ -87,18 +88,18 @@ void Logger::Logger::LogMemoryDump(int logType, void* const p_address, const int
 	const char* c = reinterpret_cast<const char*>(p_address);
 
 	sstr <<
-			"MEMDUMP\t | " <<
-			"N:[" << p_name << "]\t | " <<
-			"A:[0x" << p_address << "]\t | " <<
-			"S:" << p_size << " | " <<
-			"D:[ ";
+		"MEMDUMP\t | " <<
+		"N:[" << p_name << "]\t | " <<
+		"A:[0x" << p_address << "]\t | " <<
+		"S:" << p_size << " | " <<
+		"D:[ ";
 
-		for (int i = 0; i < p_size; ++i)
-		{
-			sstr << "0x" << std::hex << int(c[i]) << " ";
-		}
+	for (int i = 0; i < p_size; ++i)
+	{
+		sstr << "0x" << std::hex << int(c[i]) << " ";
+	}
 
-		sstr << "]";
-		Log(logType, sstr.str());
+	sstr << "]";
+	Log(logType, sstr.str());
 }
 
