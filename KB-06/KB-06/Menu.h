49a0be71de279e04fceefd18fa170a772bbf3776
hammersyko <alinor.hammersyko@gmail.com>
2014-09-25 15:34:05 +0200

    start on menu (which has to be revamped completly... old idea no longer works)

---
+++ KB-06/KB-06/Menu.h
@@ -0,0 +1,23 @@
+#ifndef __SCENE_MENU_H__
+#define __SCENE_MENU_H__
+
+#include "MenuButton.h"
+#include <vector>
+
+namespace Scene
+{
+	class Menu
+	{
+	public:
+		Menu();
+		~Menu();
+		void addButton(MenuButtonFuction function, std::string name, int top, int bottom, int left, int right, int page);
+		void click(int x, int y);
+		
+		
+
+	protected:
+		std::vector<MenuButton> buttons;		
+	};
+}
+#endif
---
+++ KB-06/KB-06/MenuButton.h
@@ -0,0 +1,25 @@
+#ifndef _RESOURCE_MENUBUTTON_H_
+#define _RESOURCE_MENUBUTTON_H_
+
+#include <string>
+
+namespace Scene
+{
+	enum MenuButtonFuction
+	{
+		EXIT, BACK, SELECT, SCENE
+	};
+
+	struct MenuButton
+	{
+		MenuButtonFuction function;
+		std::string name;
+		int top;
+		int bottom;
+		int left;
+		int right;
+		int page;
+	};
+}
+
+#endif
