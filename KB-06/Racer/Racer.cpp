02ce189cec6c7f31973e91773d60c300920dfb06
RobertAlblas <robertalblas1993@gmail.com>
2014-09-26 15:07:46 +0200

    Racer.cpp namespees

--- KB-06/Racer/Racer.cpp
+++ KB-06/Racer/Racer.cpp
@@ -14,34 +14,34 @@
 
 int main(int argc, char* argv[])
 {
-	PEngine pEngine;
-	Logger::Logger* logger = Logger::LoggerPool::GetInstance().GetInstance().GetLogger();
+	pengine::PEngine pEngine;
+	pengine::Logger* logger = pengine::LoggerPool::GetInstance().GetInstance().GetLogger();
 	pEngine.Init();
 
 	pEngine.GetWindowManager()->AddWindowListener(pEngine.GetInputManager());
 	pEngine.GetWindowManager()->NewWindow(10, 10, 500, 500);
 	pEngine.GetRenderer()->InitD3D(pEngine.GetWindowManager()->GetLastWindow()->GetHWND());
-	pEngine.GetResourceManager()->AddMeshLoader(new Resource::ObjMeshLoader());
-	pEngine.GetResourceManager()->AddMaterialLoader(new Resource::MtlLoader());
+	pEngine.GetResourceManager()->AddMeshLoader(new pengine::ObjMeshLoader());
+	pEngine.GetResourceManager()->AddMaterialLoader(new pengine::MtlLoader());
 
-	Resource::XModel* xmodel = new Resource::XModel();
-	Resource::XModelLoader* xmodelLoader = new Resource::XModelLoader();
-	xmodelLoader->LoadXModel("resources/tiger.x", static_cast<Renderer::DirectXRenderer*>(pEngine.GetRenderer()), xmodel);
+	pengine::XModel* xmodel = new pengine::XModel();
+	pengine::XModelLoader* xmodelLoader = new pengine::XModelLoader();
+	xmodelLoader->LoadXModel("resources/tiger.x", static_cast<pengine::DirectXRenderer*>(pEngine.GetRenderer()), xmodel);
 
-	Resource::XModel* xmodel2 = new Resource::XModel();
-	xmodelLoader->LoadXModel("resources/camera.x", static_cast<Renderer::DirectXRenderer*>(pEngine.GetRenderer()), xmodel2);
+	pengine::XModel* xmodel2 = new pengine::XModel();
+	xmodelLoader->LoadXModel("resources/camera.x", static_cast<pengine::DirectXRenderer*>(pEngine.GetRenderer()), xmodel2);
 
-	Resource::RGBAColor color;
+	pengine::RGBAColor color;
 	color.r = 1.0f;
 	color.g = 0.25f;
 	color.b = 1.0f;
 	color.a = 1.0f;
 
-	Resource::Mesh* mesh = pEngine.GetResourceManager()->LoadMesh("resources/cube.obj.mesh", "obj.mesh");
-	Resource::Mesh* mesh2 = pEngine.GetResourceManager()->LoadMesh("resources/cubeClone.obj.mesh", "obj.mesh");
-	Resource::Mesh* mesh3 = pEngine.GetResourceManager()->LoadMesh("resources/cubeCloneClone.obj.mesh", "obj.mesh");
+	pengine::Mesh* mesh = pEngine.GetResourceManager()->LoadMesh("resources/cube.obj.mesh", "obj.mesh");
+	pengine::Mesh* mesh2 = pEngine.GetResourceManager()->LoadMesh("resources/cubeClone.obj.mesh", "obj.mesh");
+	pengine::Mesh* mesh3 = pEngine.GetResourceManager()->LoadMesh("resources/cubeCloneClone.obj.mesh", "obj.mesh");
 
-	Scene::DefaultSceneFactory* sceneFactory = new Scene::DefaultSceneFactory();
+	pengine::DefaultSceneFactory* sceneFactory = new pengine::DefaultSceneFactory();
 	sceneFactory->setMesh(mesh);
 	sceneFactory->setMesh2(mesh2);
 	sceneFactory->setMesh3(mesh3);
@@ -49,7 +49,7 @@ int main(int argc, char* argv[])
 	sceneFactory->SetXModel2(xmodel2);
 
 	pEngine.GetSceneManager()->AddSceneFactory("iets", sceneFactory);
-	Scene::Scene* scene = pEngine.GetSceneManager()->AddScene("iets");
+	pengine::Scene* scene = pEngine.GetSceneManager()->AddScene("iets");
 	pEngine.GetSceneManager()->SetCurrentScene(scene);
 
 	pEngine.GetRenderer()->SetProjectionMatrix(M_PI / 4, 100.0f);
@@ -59,7 +59,7 @@ int main(int argc, char* argv[])
 		pEngine.GetWindowManager()->UpdateWindows();
 
 		// Logics
-		std::map<Input::Input, long>* actions = pEngine.GetInputManager()->GetCurrentActions();
+		std::map<pengine::Input, long>* actions = pEngine.GetInputManager()->GetCurrentActions();
 
 		pEngine.GetSceneManager()->UpdateActiveScene(actions);
 
@@ -72,6 +72,6 @@ int main(int argc, char* argv[])
 		pEngine.GetRenderer()->EndScene();
 		pEngine.GetRenderer()->PresentScene(pEngine.GetWindowManager()->GetLastWindow()->GetHWND());
 	}
-	Logger::LoggerPool::GetInstance().ReturnLogger(logger);
+	pengine::LoggerPool::GetInstance().ReturnLogger(logger);
 }
 
