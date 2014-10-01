79cff5ad4b888b88d83924b75e54b50e279df433
RobertAlblas <robertalblas1993@gmail.com>
2014-10-01 11:11:24 +0200

    Meer tijgertjes :tiger: :tiger:

--- KB-06/Racer/RaceSceneFactory.cpp
+++ KB-06/Racer/RaceSceneFactory.cpp
@@ -20,25 +20,25 @@ pengine::Scene* racer::RaceSceneFactory::CreateScene()
 	racecart->AddAll(7.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
 	racecart->SetXModel(xModel);
 
-	//RaceCart* racecart1 = new RaceCart();
-	//RaceCart* racecart2 = new RaceCart();
-	//RaceCart* racecart3 = new RaceCart();
+	RaceCart* racecart1 = new RaceCart();
+	RaceCart* racecart2 = new RaceCart();
+	RaceCart* racecart3 = new RaceCart();
 
-	//
-	//racecart1->AddAll(7.5f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
-	//racecart2->AddAll(7.5f, 0.0f, -5.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
-	//racecart3->AddAll(7.5f, 0.0f, 10.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
+	
+	racecart1->AddAll(7.5f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
+	racecart2->AddAll(7.5f, 0.0f, -5.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
+	racecart3->AddAll(7.5f, 0.0f, 10.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
 
 
-	//racecart1->SetXModel(xModel);
-	//racecart2->SetXModel(xModel);
-	//racecart3->SetXModel(xModel);
+	racecart1->SetXModel(xModel);
+	racecart2->SetXModel(xModel);
+	racecart3->SetXModel(xModel);
 
 	RaceScene* raceScene = new RaceScene();
 	raceScene->AddEntity(racecart);
-	//raceScene->AddEntity(racecart1);
-	//raceScene->AddEntity(racecart2);
-	//raceScene->AddEntity(racecart3);
+	raceScene->AddEntity(racecart1);
+	raceScene->AddEntity(racecart2);
+	raceScene->AddEntity(racecart3);
 
 	pengine::EntityCamera* camera = new pengine::EntityCamera();
 	camera->useInput = false;
