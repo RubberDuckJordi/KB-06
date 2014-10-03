fa706819ae9557861d208044080b22cb16605d73
RobertAlblas <robertalblas1993@gmail.com>
2014-10-03 12:34:50 +0200

    Added u and v to Vertex

--- KB-06/KB-06/Vertex.h
+++ KB-06/KB-06/Vertex.h
@@ -5,7 +5,7 @@ namespace pengine
 {
 	struct Vertex
 	{
-		float x, y, z;
+		float x, y, z, u, v;
 
 		Vertex operator+(Vertex pA)
 		{
@@ -13,6 +13,8 @@ namespace pengine
 			result.x = x + pA.x;
 			result.y = y + pA.y;
 			result.z = z + pA.z;
+			result.u = z + pA.u;
+			result.v = z + pA.v;
 			return result;
 		};
 
@@ -22,6 +24,8 @@ namespace pengine
 			result.x = x - pA.x;
 			result.y = y - pA.y;
 			result.z = z - pA.z;
+			result.u = z - pA.u;
+			result.v = z - pA.v;
 			return result;
 		};
 
@@ -31,6 +35,8 @@ namespace pengine
 			result.x = x * scalar;
 			result.y = y * scalar;
 			result.z = z * scalar;
+			result.u = u * scalar;
+			result.v = v * scalar;
 			return result;
 		};
 	};
