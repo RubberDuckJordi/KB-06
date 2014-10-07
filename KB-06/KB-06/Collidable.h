4cd2655e723a1dafc84f885ca59d220d8c0790b8
RobertAlblas <robertalblas1993@gmail.com>
2014-10-07 14:15:10 +0200

    Added mass to collidable

--- KB-06/KB-06/Collidable.h
+++ KB-06/KB-06/Collidable.h
@@ -24,7 +24,9 @@ namespace pengine
 		// Function that initializes the collisionBox, must be implemented
 		virtual void InitCollisionBox() = 0;
 		// Function returns vector with current force. This can be used for a physical reaction to the impact.
-		virtual Vector3* GetForceVector() = 0;
+		virtual Vector3* GetCollisionForceVector() = 0;
+		// Function returns mass. This can be used for a physical reaction to the impact.
+		virtual float GetCollisionMass() = 0;
 		// Must return a collision box relative to the world matrix
 		RECTANGLE* GetCollisionBox();
 		// Checks collision with another collidable, can be overridden to provide an own implementation
