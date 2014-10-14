e6040828da3e302978a64b3bbd45ca47aaab4d6a
Kevin van Leeuwen <kevin_rsx@msn.com>
2014-10-14 15:16:52 +0200

    fix voor vorige commit

--- KB-06/KB-06/ResourceManager.h
+++ KB-06/KB-06/ResourceManager.h
@@ -1,62 +1,4 @@
-<<<<<<< HEAD
-#ifndef _PENGINE_RESOURCEMANAGER_H_
-#define _PENGINE_RESOURCEMANAGER_H_
 
-#include "LoggerPool.h"
-#include "BinaryData.h"
-#include "Sound.h"
-#include "Material.h"
-#include "Ground.h"
-#include "HeightmapLoader.h"
-#include "SceneLoader.h"
-#include "SuperXLoader.h"
-#include "Shader.h"
-
-#include <vector>
-#include <string>
-
-namespace pengine
-{
-	class ResourceManager
-	{
-	public:
-		ResourceManager();
-		~ResourceManager();
-
-		//Mesh* LoadMesh(const std::string& fileName, const std::string& extension);
-		Material* LoadMaterial(const std::string& fileName, const std::string& extension);
-		BinaryData* LoadBinaryFile(const std::string& fileName);
-		Ground* LoadGround(std::string filename, std::string textureFilename);
-		Shader* LoadShader(std::string VertexShaderFilename, std::string PixelShaderFilename, std::string filename, Renderer* renderer);
-		std::vector<std::string>* LoadSceneFile(std::string* path);
-		Model3D* LoadXFile(std::string* fileName);
-
-		//Mesh* LoadSound(const std::string& fileName, const std::string& extension);
-
-		//void AddMeshLoader(BaseMeshLoader*);
-		//void AddMaterialLoader(BaseMaterialLoader*);
-		//void AddSoundLoader(BaseMeshLoader*);
-
-		//std::map<std::string, Material>* LoadMaterials(std::string file);
-	private:
-		Logger* logger;
-
-		HeightmapLoader heightmapLoader;
-		SceneLoader* sceneLoader;
-		SuperXLoader* superXLoader;
-		//std::map<std::string, Mesh> meshes;
-		std::map<std::string, Material> materials;
-		std::map<std::string, BinaryData> textures;
-		std::map<std::string, Sound> sounds;
-		std::map<std::string, Ground> grounds;
-		std::map<std::string, Shader> shaders;
-
-		//std::map<std::string, BaseMeshLoader*> meshLoaders;
-		//std::map<std::string, BaseMaterialLoader*> materialLoaders;
-	};
-}
-#endif
-=======
 #ifndef _PENGINE_RESOURCEMANAGER_H_
 #define _PENGINE_RESOURCEMANAGER_H_
 
@@ -86,7 +28,7 @@ namespace pengine
 		Material* LoadMaterial(const std::string& fileName, const std::string& extension);
 		BinaryData* LoadBinaryFile(const std::string& fileName);
 		Ground* LoadGround(std::string filename, std::string textureFilename);
-		Shader* LoadShader(std::string filename, Renderer* renderer);
+		Shader* LoadShader(std::string VertexShaderFilename, std::string PixelShaderFilename, std::string filename, Renderer* renderer);
 		std::vector<std::string>* LoadSceneFile(std::string* path);
 		Object3D* LoadXFile(std::string* fileName);
 
@@ -108,6 +50,7 @@ namespace pengine
 		std::map<std::string, BinaryData> textures;
 		std::map<std::string, Sound> sounds;
 		std::map<std::string, Ground> grounds;
+		std::map<std::string, Shader> shaders;
 		std::map<std::string, Model3D*> models;
 
 		//std::map<std::string, BaseMeshLoader*> meshLoaders;
@@ -115,4 +58,3 @@ namespace pengine
 	};
 }
 #endif
->>>>>>> origin/master
