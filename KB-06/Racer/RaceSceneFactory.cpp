e61ac22a160a29ed5509e98c8ccba91caccc1290
ManIkWeet <spamme2010@live.nl>
2014-10-15 23:49:14 +0200

    Stupid GitHub program being :snail:

--- KB-06/Racer/RaceSceneFactory.cpp
+++ KB-06/Racer/RaceSceneFactory.cpp
@@ -210,13 +210,8 @@ pengine::Scene* racer::RaceSceneFactory::CreateScene()
 
 	RaceCart* racecart1 = new RaceCart();
 	racecart1->SetMass(100.0f);
-	RaceCart* racecart2 = new RaceCart();
-	racecart2->SetMass(100.0f);
-
 	racecart1->AddAll(7.5f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
-	racecart2->AddAll(20.5f, 0.0f, -10.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
 	racecart1->SetObject3D(object3d2);
-	racecart2->SetObject3D(object3d2);
 
 
 	Track* track = new Track();
@@ -249,10 +244,8 @@ pengine::Scene* racer::RaceSceneFactory::CreateScene()
 	RaceScene* raceScene = new RaceScene();
 	raceScene->AddEntity(racecart);
 	raceScene->AddEntity(racecart1);
-	//raceScene->AddEntity(racecart2);
 	raceScene->AddCollidable(racecart);
 	raceScene->AddCollidable(racecart1);
-	//raceScene->AddCollidable(racecart2);
 	raceScene->AddEntity(track);
 	raceScene->SetGround(ground);
 	raceScene->SetSkybox(skybox);
